e3a1696f27142273f52f3b2c428d3bbecec9fb16
59013-90-05 <59013-90-05@exchange.ad.afpanet>
2023-02-22 14:12:59 +0100

    add infractionStyle

--- MVC/view/infractionView.php
+++ MVC/view/infractionView.php
@@ -4,27 +4,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../view/css/infractionStyle.css">
     <title>Document</title>
 </head>
 <body>
     <?php session_start(); ?>
 
-    <table class = infraction>
-        <thead>
-            <th>Petit Juron</th>
-            <th>Gros Juron</th>
-            <th>Geste</th>
-            <th>Rôt</th>
-            <th>Retard</th>
-        </thead>
-        <tbody>
-        <?php for ($i=0; $i < count($_SESSION['infraction']); $i++) { 
+<table class = "radioGaga">
+    
+      <th> <input type="radio" id="pj" value="pj" name="inf"> 
+      <label for="">Petit Juron</label> </th>
+
+      <th> <input type="radio" id="gj" value="gj" name="inf"> 
+      <label for="">Gros Juron</label> </th>
+
+      <th> <input type="radio" id="ge" value="ge" name="inf"> 
+      <label for="">Geste</label> </th>
+      
+      <th> <input type="radio" id="ro" value="ro" name="inf"> 
+      <label for="">Rôt</label> </th>
+      
+      <th> <input type="radio" id="re" value="re" name="inf"> 
+      <label for="">Retard</label> </th>
+      
+
+</table>
+
+<table class = "infraUser">
+
+        <?php for ($i=0; $i < count($_SESSION['utilisateur']); $i++) { 
+
            echo 'tr class ="">';
-           echo 'td class ="">'.$_SESSION['infraction'][$i]['nom'];
-           echo 'td class ="">'.$_SESSION['infraction'][$i]['prenom'];
-           echo 'td class ="">'.$_SESSION['infraction'][$i]['solde'];
+           echo 'td class ="">'.$_SESSION['utilisateur'][$i]['nom'];
+           echo 'td class ="">'.$_SESSION['utilisateur'][$i]['prenom'];
+           echo 'td class ="">'.$_SESSION['utilisateur'][$i]['solde'];
+
         }?>
-        </tbody>
+   
     </table>
 </body>
 </html>
